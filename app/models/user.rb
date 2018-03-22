4b0233622e8c9345300f6799f142c9271775aaa5
woolsox <dimeo.colin@gmail.com>
2018-03-22 09:31:35 -0400

    adds avatar to user, removes image uploader and swaps to avatar uploader, adds in devise controllers

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base
-  mount_uploader :image, ImageUploader
+  mount_uploader :avatar, AvatarUploader
 
-  delegate :url, to: :image, allow_nil: true
+  delegate :url, to: :avatar, allow_nil: true
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
