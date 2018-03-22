62c87ef1fd92066539fd1d88132326287c720e6e
woolsox <dimeo.colin@gmail.com>
2018-03-22 11:01:59 -0400

    removes parameter rigidity, updates user model, and reg/sess controllers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,14 @@
 class ApplicationController < ActionController::API
   include DeviseTokenAuth::Concerns::SetUserByToken
+  wrap_parameters format: []
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  private
+
+  def configure_permitted_parameters
+    attributes = %i[email password password_confirmation avatar url]
+    devise_parameter_sanitizer.permit(:sign_in, keys: attributes)
+    devise_parameter_sanitizer.permit(:sign_up, keys: attributes)
+    devise_parameter_sanitizer.permit(:account_update, keys: attributes)
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ActiveRecord::Base
   mount_uploader :avatar, AvatarUploader
 
+  attr_accessor :url
+
   delegate :url, to: :avatar, allow_nil: true
 
   devise :database_authenticatable, :registerable,
